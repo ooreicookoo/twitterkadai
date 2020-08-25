a140f821778e1da419356afa8df2f1540bf73fb6
Reiko Enomoto <ooreicookoo@gmail.com>
2020-08-25 23:25:28 +0900

    WIP コンテンツが入力出来るところまで

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,4 +1,24 @@
 class AppsController < ApplicationController
   def index
+    @app = App.new(app_params)
   end
+
+  def show
+  end
+
+  def new
+    @app = App.new
+  end
+
+  def create
+    App.create(content: params[:app][:content])
+    redirect_to "/apps/new"
+  end
+
+  def edit
+  end
+
+  def confirm
+  end
+
 end
