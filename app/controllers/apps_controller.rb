f1889cf5f9fcd4467115aa597ab5df72c51fc44f
Reiko Enomoto <ooreicookoo@gmail.com>
2020-08-26 09:43:45 +0900

    WIP ストロングパラメーターをメゾットとして定義

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -11,14 +11,19 @@ class AppsController < ApplicationController
   end
 
   def create
-    App.create(content: params[:app][:content])
-    redirect_to "/apps/new"
+    App.create(app_params)
+    redirect_to new_app_path
   end
 
   def edit
   end
 
   def confirm
+  end
+
+  private
+  def app_params
+    params.require(:app).permit(:content)
   end
 
 end
