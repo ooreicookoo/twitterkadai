b65e0c6b5987c405e9209f57e993cfa54c1d6840
Reiko Enomoto <ooreicookoo@gmail.com>
2020-09-03 13:20:24 +0900

    fix2:ヘルパーメゾットにコンファーム追記

--- app/helpers/apps_helper.rb
+++ app/helpers/apps_helper.rb
@@ -1,6 +1,6 @@
 module AppsHelper
   def choose_new_or_edit
-    if action_name == 'new' || action_name == 'create'
+    if action_name == 'new' || action_name == 'confirm' || action_name =='create'
       confirm_apps_path
     elsif action_name == 'edit'
       app_path
